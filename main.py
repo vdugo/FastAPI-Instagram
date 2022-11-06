f3141d0b04e3e722fb40888ce6b5d2600b1f54c2
Vincent <v3rdugo@yahoo.com>
2022-11-06 15:18:56 -0800

    set up database and model

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+
+from db import models
+from db.database import engine
 
 app = FastAPI()
 
 @app.get('/')
 def home_page():
     return {"message": "this is the homepage"}
+
+models.Base.metadata.create_all(engine)
