99b5e72434021eab2acc484cea73f203694effb1
Vincent <v3rdugo@yahoo.com>
2022-11-06 16:24:02 -0800

    create model for a database post, add relationship between a user and a post

--- db/models.py
+++ db/models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 
-from .database import  Base
+from .database import Base
 
 class DbUser(Base):
     __tablename__ = 'user'
@@ -8,3 +10,14 @@ class DbUser(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+    items = relationship('DbPost', back_populates='user')
+    
+class DbPost(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True, index=True)
+    image_url = Column(String)
+    image_url_type = Column(String)
+    caption = Column(String)
+    timestamp = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('DbUser', back_populates='items')
