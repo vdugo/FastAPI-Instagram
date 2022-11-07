10990503804e8c6a7e94d476175feb2bb7df3ed4
Vincent <v3rdugo@yahoo.com>
2022-11-07 15:14:22 -0800

    generated random secret using 'openssl rand -hex 32' will change to environment variables later

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -11,7 +11,7 @@ from db import db_user
  
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
  
-SECRET_KEY = '77407c7339a6c00544e51af1101c4abb4aea2a31157ca5f7dfd87da02a628107'
+SECRET_KEY = 'a16fe0f757003b224bf230fdd63e2b9551a0419881055e777631a37e23adcd53'
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
  
