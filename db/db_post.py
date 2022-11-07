bfe62fd880fab38cb063973a235cc8f6360a8023
Vincent <v3rdugo@yahoo.com>
2022-11-07 15:45:01 -0800

    only owner of post can delete a post

--- db/db_post.py
+++ db/db_post.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from fastapi import HTTPException, status
 from sqlalchemy.orm.session import Session
 
 from db.models import DbPost
@@ -21,3 +22,17 @@ def create(db: Session, request: PostBase):
 
 def get_all(db: Session):
     return db.query(DbPost).all()
+
+def delete(db: Session, id: int, user_id: int):
+    post = db.query(DbPost).filter(DbPost.id == id).first()
+    
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found.")
+    
+    if post.user_id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Only post creator can delete posts.")
+    
+    db.delete(post)
+    db.commit()
+    
+    return post
