925462fce9e664a594413080028077ea540db4c2
Vincent <v3rdugo@yahoo.com>
2022-11-06 16:11:23 -0800

    hash the password when creating a new user in the database

--- db/db_user.py
+++ db/db_user.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm.session import Session
 
+from db.hashing import Hash
 from .models import DbUser
 from routers.schemas import UserBase
 
@@ -7,7 +8,8 @@ def create_user(db: Session, request: UserBase):
     new_user = DbUser(
         username = request.username,
         email = request.email,
-        password = request.password
+        # store the hashed password
+        password = Hash.bcrypt(request.password)
     )
     
     db.add(new_user)
