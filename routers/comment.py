4ff18846a01271e48bc64ba9d3747c7bb98f7fac
Vincent <v3rdugo@yahoo.com>
2022-11-11 21:32:30 -0800

    fix bug in getting all comments, forgot return statement

--- routers/comment.py
+++ routers/comment.py
@@ -13,7 +13,7 @@ router = APIRouter(
 
 @router.get('/all/{post_id}')
 def comments(post_id: int, db: Session = Depends(get_db)):
-    db_comment.get_all(db, post_id)
+    return db_comment.get_all(db, post_id)
     
 @router.post('/')
 def create(request: CommentBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
